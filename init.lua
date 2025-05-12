c60d5c3cfe1d7e093132ccc1ffabfa994cfd568e
tigersharke <tigersharke@gmail.com>
2025-05-12 16:01:25 -0500

    Allow braced forms to be attached.

--- init.lua
+++ init.lua
@@ -21,3 +21,16 @@ minetest.override_item("nc_woodwork:form",
 	}}
 )
 
+-- Permit both styles of form to be attached to creel handle.
+minetest.override_item("nc_woodwork:form_braced",
+	{groups = {
+		basketable = 1,
+		choppy = 1,
+		flammable = 2,
+		fire_fuel = 1,
+		totable = 1,
+		storebox = 1,
+		visinv = 1
+	}}
+)
+
